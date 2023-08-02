7dd3a4e0e03b5903a51eba6529a2fbe0cf247c90
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-08-02 22:36:46 +1000

    Nix testing

---
+++ shell.nix
@@ -0,0 +1,37 @@
+let
+    rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+    pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+    rust_version = "1.61.0";
+    rust = pkgs.rust-bin.stable.${rust_version}.default.override {
+        targets = [ "x86_64-unknown-linux-musl" ];
+    };
+    tex = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-medium titlesec;
+    };
+    cross = import <nixpkgs> {
+        crossSystem = { config = "aarch64-none-elf"; };
+    };
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs.buildPackages; [
+        qemu
+        gnumake
+        dtc
+        expect
+        python39
+        git
+        gcc
+        rust
+        gmp.out
+        pandoc
+        tex
+        cmake
+        cross.buildPackages.gcc10
+        ninja
+        libxml2
+    ];
+    shellHook = ''
+        export PYOXIDIZER_SYSTEM_RUST=1
+    '';
+}
+
